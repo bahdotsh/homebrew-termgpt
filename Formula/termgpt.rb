6c6ea729c0468f3d30a6f0a181df0328049519bd
bahdotsh <appu.yess@gmail.com>
2023-05-08 19:56:58 +0530

    v0.1.1

--- Formula/termgpt.rb
+++ Formula/termgpt.rb
@@ -1,9 +1,9 @@
 class Termgpt < Formula
   desc "A cli tool to interact with ChatGPT"
   homepage "https://github.com/bahdotsh/termgpt"
-  url "https://github.com/bahdotsh/termgpt/releases/download/v0.1.0/termgpt-mac.tar.gz"
-  sha256 "72dc67b4eb1814a85d260578117b59330f2fc551bd0032aacbec83cc01c6ad0a"
-  version "0.1.0"
+  url "https://github.com/bahdotsh/termgpt/releases/download/v0.1.1/termgpt-mac.tar.gz"
+  sha256 "d3357dd3f02cb2071b8c253debd7e3b54e8c4de1fcd11451e0c4ba52213800f5"
+  version "0.1.1"
 
   def install
     bin.install "termgpt"
